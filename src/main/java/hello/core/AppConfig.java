722794ef7c88fc6633ce4231465ec5a78ba5d7b3
zerojuu <zerojuu@naver.com>
2022-09-19 23:20:58 +0900

    AppConfig 리팩터링(중복 제거 및 역할 보이게)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,10 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemmoryMemberRepository;
+import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -12,11 +10,21 @@ public class AppConfig {
 
     //memberservice 생성 -> memberserviceimpl에서 MemberRepository만 선언 및 생성자 생성 -> 생성자를 통해서 MemmoryMemberRepository 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemmoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    //중복 제거 및 역할 보이게 리팩터링
+    private MemberRepository memberRepository() {
+        return new MemmoryMemberRepository();  //리턴 타입은 인터페이스로 해야 함
     }
 
     //orderservice 생성
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemmoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    //중복 제거 및 역할 보이게 리팩터링
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }

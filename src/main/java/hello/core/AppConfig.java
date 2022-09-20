867f14315d57fdda21d26f2f002aeb30b4027a6f
zerojuu <zerojuu@naver.com>
2022-09-20 23:21:23 +0900

    스프링으로 전환(+AppConfig에서 private -> public 오타 수정)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,25 +6,32 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  //설정정보
 public class AppConfig {
 
     //memberservice 생성 -> memberserviceimpl에서 MemberRepository만 선언 및 생성자 생성 -> 생성자를 통해서 MemmoryMemberRepository 주입
+    @Bean  //bean 선언시 스프링 컨테이너에 자동등록이 됨
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     //중복 제거 및 역할 보이게 리팩터링
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemmoryMemberRepository();  //리턴 타입은 인터페이스로 해야 함
     }
 
     //orderservice 생성
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //중복 제거 및 역할 보이게 리팩터링
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();  //FixDiscountPolicy에서 RateDiscountPolicy로 변경 원하면 여기만 수정하면 됨
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();  //memberServiceImpl이 들어가있는 memberService
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();  //memberServiceImpl이 들어가있는 memberService
         //MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);  //ApplicationContext:스프링 컨테이너, AppConfig에 있는 환경설정으로 적용
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);  //AppConfig에서 꺼낼 객체 이름, 타입
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);  //회원가입
 

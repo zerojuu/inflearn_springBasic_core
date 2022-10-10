ed8dc4256586c916a3772004e2dc08109832b295
zerojuu <zerojuu@naver.com>
2022-10-10 22:50:41 +0900

    싱글톤 방식의 주의(무상태 stateless)

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,22 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+//    private int price;  //상태를 유지하는 필드
+//
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + "price = " + price);
+//        this.price = price;  //여기가 문제!
+//    }
+
+    //무상태로 설계
+    public int order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        return price;
+    }
+
+    //무상태로 설계위해 주석 처리
+//    public int getPrice() {
+//        return price;
+//    }
+}
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,59 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+       ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+       
+       //시나리오 : A사용자가 주문을 하고 금액을 조회하려는 사이 B사용자가 주문을 중간에 해버림
+       
+       //조회
+       StatefulService statefulService1 = ac.getBean(StatefulService.class);
+       StatefulService statefulService2 = ac.getBean(StatefulService.class);
+       
+       //ThreadA : A사용자 10000원 주문 
+        statefulService1.order("userA", 10000);
+       //ThreadB : B사용자 20000원 주문
+       statefulService2.order("userB", 20000);
+        
+        //ThreadA : A사용자 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);  //service를 1,2로 나누어도 인스턴스는 같기 때문에 마지막인 B사용자의 20000원이 조회됨
+
+        //검증
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    //무상태로 재설계
+    @Test
+    void statelessServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        //조회
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA : A사용자 10000원 주문 -> int(지역변수) 사용
+        int userAprice = statefulService1.order("userA", 10000);
+        //ThreadB : B사용자 20000원 주문
+        int userBprice = statefulService2.order("userB", 20000);
+
+        //ThreadA : A사용자 주문 금액 조회
+        System.out.println("price = " + userAprcie);
+    }
+    
+    //테스트용 config
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}

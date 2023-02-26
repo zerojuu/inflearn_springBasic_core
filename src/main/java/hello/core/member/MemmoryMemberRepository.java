08f1cc7621b8365ec7e58988eac2a0824efca8b2
zerojuu <zerojuu@naver.com>
2023-02-26 16:00:52 +0900

    리마인드 (회원 도메인 개발)

--- src/main/java/hello/core/member/MemmoryMemberRepository.java
+++ src/main/java/hello/core/member/MemmoryMemberRepository.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MemmoryMemberRepository implements MemberRepository{
 
+    //데이터베이스가 미확정이라 memorymemberrepository 생성 (메모리에서만 하기 때문에 개발용으로는 상관없음)
     //회원저장소니까 map같은 게 필요
     private static Map<Long, Member> store = new HashMap<>(); //실무에서는 동시성 이슈로 인해 HashMap이 아닌 ConcurrentHashMap을 써야 함
 

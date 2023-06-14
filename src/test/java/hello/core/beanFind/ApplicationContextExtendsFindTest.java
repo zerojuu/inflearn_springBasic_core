b7108b077556fb8148a89ee9bef29553ff2c0cf3
zerojuu <zerojuu@naver.com>
2023-06-14 11:03:11 +0900

    리마인드 (스프링 빈 조회 - 상속 관계)

--- src/test/java/hello/core/beanFind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanFind/ApplicationContextExtendsFindTest.java
@@ -17,12 +17,26 @@ import java.util.Stack;
 
 public class ApplicationContextExtendsFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+    //내부 config
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);   //내부 config 지정
 
     @Test
     @DisplayName("부모 타입으로 조회시 자식이 둘 이상 있으면 중복 오류가 발생한다")
     void findBeanByParentTypeDuplicate() {
-        //DiscountPolicy bean = ac.getBean(DiscountPolicy.class);  //실행시 NoUniqueBeanDefinitionException 발생
+        //DiscountPolicy bean = ac.getBean(DiscountPolicy.class);  //실행시 rate,fixDiscountPolicy 두개 존재하여 NoUniqueBeanDefinitionException 발생
         Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(DiscountPolicy.class));
     }
@@ -30,14 +44,14 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시 자식이 둘 이상 있으면 빈 이름을 지정하면 된다")
     void findBeanByParentTypeBeanName() {
-        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class); //빈 이름(rateDiscountPolicy) 지정
         org.assertj.core.api.Assertions.assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
     @DisplayName("특정 하위 타입으로 조회")  //좋은 방법은 아님
     void findBeanBySubType() {
-        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);  //특정 타입을 지정
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);  //특정 타입(RateDiscountPolicy)을 지정
         org.assertj.core.api.Assertions.assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
@@ -61,18 +75,6 @@ public class ApplicationContextExtendsFindTest {
         //출력
         for (String key : beansOfType.keySet()) {
             System.out.println("key  = " + key + "value = " + beansOfType.get(key));
-        }
-    }
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public DiscountPolicy rateDiscountPolicy() {
-            return new RateDiscountPolicy();
-        }
-
-        @Bean
-        public DiscountPolicy fixDiscountPolicy() {
-            return new FixDiscountPolicy();
         }
     }
 }
--- src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
@@ -16,10 +16,12 @@ import java.util.Map;
 
 class ApplicationContextSameBeanFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);   //내부에서 쓰기로 생성한 SameBeanConfig 선언
 
+    //static 장점 : class 안에 class를 썼다(중첩클래스)는 건 class 안에서만 쓰겠다는.. scope
     @Configuration  //ApplicationContextSameBeanFindTest 내부에서만 쓸 config
     static class SameBeanConfig {
+        //빈의 이름(memberRepository1,2)이 다르고 객체 타입(MemmoryMemberRepository)은 같을 수 있음 : 파라미터 값을 넣을 수도 있음 return new MemmoryMemberRepository("10"); 이런 식으로
         @Bean
         public MemberRepository memberRepository1() {
             return new MemmoryMemberRepository();
@@ -34,15 +36,15 @@ class ApplicationContextSameBeanFindTest {
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면 중복 오류가 발생한다")
     void findBeanByDuplicate() {
-        MemberRepository bean = ac.getBean(MemberRepository.class);  //실행시 NoUniqueBeanDefinitionException 오류 발생
+        //MemberRepository bean = ac.getBean(MemberRepository.class);  //실행시 memberRepository1,2 두개가 존재하기에 NoUniqueBeanDefinitionException 오류 발생
         Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
-                () -> ac.getBean(MemberRepository.class));
+                () -> ac.getBean(MemberRepository.class));  //람다
     }
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면 빈 이름을 지정하면 된다")
     void findBeanByName() {
-        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);  //빈 이름 지정
+        MemberRepository memberRepository = ac.getBean("memberRepository1",MemberRepository.class);  //빈 이름(memberRepository1) 지정
         org.assertj.core.api.Assertions.assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 

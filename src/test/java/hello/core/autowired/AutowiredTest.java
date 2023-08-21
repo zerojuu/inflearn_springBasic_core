61a4260343bf7e17ffd991ce2fe6824ec3d358fc
zerojuu <zerojuu@naver.com>
2023-08-21 10:28:10 +0900

    섹션7 / 옵션 처리

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,40 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption () {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);  //TestBean을 넣으면 스프링 빈으로 등록됨
+    }
+
+    //임의의 테스트 클래스 생성
+    static class TestBean{
+
+        //대상자가 없기에 호출 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        //Optional.empty 호출
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}

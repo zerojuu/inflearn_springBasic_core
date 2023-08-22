0fe2d7070422c5b238f82c7b7ba3b51563153153
zerojuu <zerojuu@naver.com>
2023-08-22 14:32:41 +0900

    @RequiredArgsConstructor 관련 에러(incompatible types) 수정 : 생성자 오버로딩으로 순서대로 선언 필요

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -18,8 +18,8 @@ public class OrderServiceImpl implements OrderService{
     //private final MemberRepository memberRepository = new MemmoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  //할인정책을 변경하려고 보니 추상과 구현 클래스 둘 다 의존하고 있음 -> DIP 위반
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();  //할인정책 변경
-    private final DiscountPolicy discountPolicy;  //final은 무조건 값이 할당되어야 하기 때문에 아닌 경우 삭제, discountpolicy라는 인터페이스만 의존하게 됨 -> 생성자 주입을 사용하면 필드에 final 사용 가능
     private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;  //final은 무조건 값이 할당되어야 하기 때문에 아닌 경우 삭제, discountpolicy라는 인터페이스만 의존하게 됨 -> 생성자 주입을 사용하면 필드에 final 사용 가능
 
     //생성자.. 하나인 경우 @Autowired 생략 가능
     //MemberRepository, DiscountPolicy에 구현체가 뭐가 들어갈지는 생성자를 통해서 감
